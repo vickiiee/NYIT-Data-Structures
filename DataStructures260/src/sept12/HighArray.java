98938f3076410f4a37165fece884df317e5681ca
VJ <VJ@216.37.101.27>
2019-09-12 15:44:07 -0400

    finish getMax() 2.1

--- DataStructures260/src/sept12/HighArray.java
+++ DataStructures260/src/sept12/HighArray.java
@@ -6,21 +6,21 @@ package sept12;
 //demonstrates array class with high-level interface
 //to run this program: C>java HighArrayApp
 
-class HighArray {
+public class HighArray {
 	private long[] a; // ref to array a
 	private int nElems; // number of data items
-	
+
 	private int foundIndex;
 //-----------------------------------------------------------
 
 	public HighArray(int max) // constructor
 	{
 		a = new long[max]; // create the array
-		/**for(int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
-		}**/
+		/**
+		 * for(int i = 0; i < a.length; i++) { System.out.println(a[i]); }
+		 **/
 		nElems = 0; // no items yet
-		
+
 		foundIndex = 0;
 	}
 
@@ -29,17 +29,16 @@ class HighArray {
 		int j;
 		for (j = 0; j < nElems; j++) { // for each element,
 			if (a[j] == searchKey) { // found item?
-				//break; // exit loop before end
+				// break; // exit loop before end
 				foundIndex = j;
 				return true;
 			}
 		}
-		/**if (j == nElems) // gone to end?
-			return false; // yes, can't find it
-		else
-			return true; // no, found it
-			**/
-		
+		/**
+		 * if (j == nElems) // gone to end? return false; // yes, can't find it else
+		 * return true; // no, found it
+		 **/
+
 		return false;
 	} // end find()
 //-----------------------------------------------------------
@@ -52,34 +51,37 @@ class HighArray {
 
 //-----------------------------------------------------------
 	public boolean delete(long value) {
-		/**int j;
-		for (j = 0; j < nElems; j++) // look for it
-			if (value == a[j])
-				break;
-		if (j == nElems) // can't find it
-			return false;
-		else // found it
-		{
-			for (int k = j; k < nElems; k++) // move higher ones down
-				a[k] = a[k + 1];
-			nElems--; // decrement size
-			return true;
-		}**/
-		
+		/**
+		 * int j; for (j = 0; j < nElems; j++) // look for it if (value == a[j]) break;
+		 * if (j == nElems) // can't find it return false; else // found it { for (int k
+		 * = j; k < nElems; k++) // move higher ones down a[k] = a[k + 1]; nElems--; //
+		 * decrement size return true; }
+		 **/
+
 		boolean found = find(value);
-		if(found == true) {
-			//remove it
+		if (found == true) {
+			// remove it
 			a[foundIndex] = 0;
-			for(int i = foundIndex; i<nElems; i++) { //43,1,2,3,4,0,0,0 = 5 elements, 8 cells
-				a[i]=a[i+1];
+			for (int i = foundIndex; i < nElems; i++) { // 43,1,2,3,4,0,0,0 = 5 elements, 8 cells
+				a[i] = a[i + 1];
 			}
-			a[nElems] = 0; 
-			nElems = nElems-1;
-		}else {
+			a[nElems] = 0;
+			nElems = nElems - 1;
+		} else {
 			return false;
 		}
 		return false;
 	} // end delete()
+
+	public long getMax() {
+		long max = -1;
+		for(int i = 0; i < nElems; i++) {
+			if(a[i]>max) {
+				max = a[i];
+			}
+		}
+		return max;
+	}
 //-----------------------------------------------------------
 
 	public void display() // displays array contents
@@ -88,6 +90,6 @@ class HighArray {
 			System.out.print(a[j] + " "); // display it
 		System.out.println("");
 	}
+
 //-----------------------------------------------------------
-} // end class HighArray
-////////////////////////////////////////////////////////////////
+}
