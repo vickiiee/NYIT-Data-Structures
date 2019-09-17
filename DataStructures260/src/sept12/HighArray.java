95d7a7237fd79916fc2e706cc2cd7f4e96b05d75
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-09-17 02:23:39 -0400

    .getMax() finished

--- DataStructures260/src/sept12/HighArray.java
+++ DataStructures260/src/sept12/HighArray.java
@@ -3,102 +3,68 @@ package sept12;
 
 //9/12/19
 
-//highArray.java
-//demonstrates array class with high-level interface
-//to run this program: C>java HighArrayApp
-
 public class HighArray {
-	private long[] a; // ref to array a
+	private long[] a;
 	private int nElems; // number of data items
-
 	private int foundIndex;
-	
 	private int maxSize;
-//-----------------------------------------------------------
 
-	public HighArray(int max) { // constructor
+	public HighArray(int max) {
 		if (max <= 0) {
 			throw new IllegalArgumentException("size must be > 0");
 		}
-		
+
 		a = new long[max]; // create the array
 		maxSize = max;
-		
-		/**
-		 * for(int i = 0; i < a.length; i++) { System.out.println(a[i]); }
-		 **/
 		nElems = 0; // no items yet
-
 		foundIndex = 0;
 	}
 
-//-----------------------------------------------------------
+	public void insert(long value) { // put element into array
+		if (nElems == maxSize) {
+			System.out.println("Array is full. Cannot add " + value);
+		} else {
+			a[nElems] = value; // insert it
+			nElems++; // # of elements increased
+		}
+	}
+
+	// ---------------2.0-----------------------------------
 	public boolean find(long searchKey) { // find specified value
 		int j;
 		for (j = 0; j < nElems; j++) { // for each element,
 			if (a[j] == searchKey) { // found item?
-				// break; // exit loop before end
 				foundIndex = j;
 				return true;
 			}
 		}
-		/**
-		 * if (j == nElems) // gone to end? return false; // yes, can't find it else
-		 * return true; // no, found it
-		 **/
-
 		return false;
-	} // end find()
-//-----------------------------------------------------------
-
-	public void insert(long value){ // put element into array
-		if(nElems ==maxSize) {
-			System.out.println("Array is full. Cannot add "+ value);
-		} else {
-			a[nElems] = value; // insert it
-			nElems++; // increment size
-		}
 	}
 
-//-----------------------------------------------------------
 	public boolean delete(long value) {
-		/**
-		 * int j; for (j = 0; j < nElems; j++) // look for it 
-		 * 	if (value == a[j]) 
-		 * 	break;
-		 * if (j == nElems) // can't find it return false; else // found it { for (int k
-		 * = j; k < nElems; k++) // move higher ones down a[k] = a[k + 1]; nElems--; //
-		 * decrement size return true; }
-		 **/
-
 		boolean found = find(value);
+
 		if (found == true) {
-			System.out.println("Deleting "+value);
-			// remove it
-//			a[foundIndex] = 0;
-			for (int i = foundIndex; i < nElems-1; i++) { // 43,1,2,3,4,0,0,0 = 5 elements, 8 cells
+			System.out.println("Deleting " + value);
+
+			for (int i = foundIndex; i < nElems - 1; i++) { // 43,1,2,3,4,0,0,0 = 5 elements, 8 cells
 				a[i] = a[i + 1];
 			}
-			
-			a[nElems-1] = 0;
-			nElems = nElems - 1;
-			
-			/**for (int i = 0; i < a.length; i++) { // 43,1,2,3,4,0,0,0 = 5 elements, 8 cells
-				System.out.println(i+": "+a[i]);
-			}**/ //testing
-		} else {
-			System.out.println(value+" not found.");
-			return false;
-		}
-		return false;
-	} // end delete()
 
-	public long getMax() {
-		if(nElems == 0) {
-			return -1;
+			a[nElems - 1] = 0;
+			nElems = nElems - 1;
+			return true;
+		} else {
+			System.out.println(value + " not found.");
 		}
-		
+
+		return false;
+	}
+
+	// ----------2.1-----------------------------------
+	public long getMax() {
 		long max = -1;
+		
 		for (int i = 0; i < nElems; i++) {
 			if (a[i] > max) {
 				max = a[i];
@@ -107,14 +73,15 @@ public class HighArray {
 		return max;
 	}
 
-	public void removeMax() {
+	// ---------2.2------------------
+	public long removeMax() {
 		long max = getMax();
-		System.out.println("Max:"+ max);
+		System.out.println("Max:" + max);
 		delete(max);
 		display();
+		return max;
 
 	}
-//-----------------------------------------------------------
 
 	public void display() // displays array contents
 	{
@@ -122,6 +89,4 @@ public class HighArray {
 			System.out.print(a[j] + " "); // display it
 		System.out.println("");
 	}
-
-//-----------------------------------------------------------
 }
--- DataStructures260/src/sept12/HighArrayApp.java
+++ DataStructures260/src/sept12/HighArrayApp.java
@@ -1,38 +1,66 @@
 package sept12;
 
+import java.util.Arrays;
+
+//Vickie Wu
+//9/12/19
 public class HighArrayApp {
-	
-	public HighArrayApp() {
-		
-	}
+
 	public static void main(String[] args) {
-		int maxSize = 100; // array size
-		HighArray arr; // reference to array
-		arr = new HighArray(maxSize); // create the array
+		int maxSize = 100; 
+		HighArray arr = new HighArray(maxSize); 
 
-		arr.insert(77); // insert 10 items
-		arr.insert(99);
-		arr.insert(44);
-		arr.insert(55);
-		arr.insert(22);
-		arr.insert(88);
-		arr.insert(11);
-		arr.insert(00);
-		arr.insert(66);
-		arr.insert(33);
+		//arr.insert(77);
+		//arr.insert(99);
+		//arr.insert(44);
+		//arr.insert(55);
+		//arr.insert(22);
+		//arr.insert(88);
+		//arr.insert(11);
+		//arr.insert(00);
+		//arr.insert(66);
+		//arr.insert(33);
 
-		arr.display(); // display items
-
-		int searchKey = 35; // search for item
+		arr.display();
+		
+		//----test .find()----
+		System.out.println(arr.find(33));
+		
+		int searchKey = 35;
 		if (arr.find(searchKey))
 			System.out.println("Found " + searchKey);
 		else
 			System.out.println("Can't find " + searchKey);
-
-		arr.delete(00); // delete 3 items
+		
+		//----test .getMax-----
+		System.out.println("Max: "+arr.getMax());
+		
+		arr.delete(00); 
 		arr.delete(55);
 		arr.delete(99);
 
-		arr.display(); // display items again
-	} // end main()
-} // end class HighArrayApp
+		arr.display();
+
+//-------------2.3----Sorting using .removeMax();---------------------------------------
+
+		long[] inverse = new long[maxSize];
+		int index = 0;
+		/*while (arr.getMax() != -1) {
+			inverse[index] = arr.getMax();
+			arr.removeMax();
+			index++;
+		}*/
+
+		while (arr.getMax() != -1) {
+			inverse[index] = arr.removeMax();
+			index++;
+		}
+		
+		//System.out.println(Arrays.toString(inverse));
+
+		for (int i = 0; i < index; i++) {
+			System.out.print(inverse[i] + " ");
+		}
+
+	}
+}
