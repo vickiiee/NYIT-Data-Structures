aa21e6d800e8d4e94691e2a3389600ca7bb85c09
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-09-19 12:36:58 -0400

    binary search

---
+++ DataStructures260/src/sept17/Notes.java
@@ -0,0 +1,14 @@
+package sept17;
+
+public class Notes {
+
+	public Notes() {
+		// TODO Auto-generated constructor stub
+		/**
+		 * Ordered arrays 
+		 * 
+		 * BInary search: look at middle index and constantly going to middle index to find element
+		 */
+	}
+
+}
