4cca8d142f485a3eb1eb03ef72a73cc85c76f8f2
VJ <VJ@216.37.100.27>
2019-09-24 17:24:31 -0400

    selection/insert/shell/merge sort

---
+++ DataStructures260/src/sept24/Notes.java
@@ -0,0 +1,27 @@
+package sept24;
+
+public class Notes {
+
+	public Notes() {
+		// TODO Auto-generated constructor stub
+		/**
+		 * Ch3 simple sorting pdf
+		 * Look at orcmath:
+		 * 
+		 * Selection Sort:
+		 * 	similar to bubble sort, but the sort looks for the shortest/lowest element and swap so that the left is sorted
+		 * 
+		 * Insertion Sort:
+		 * 	ex: a player sorts their hand of cards by determining where a card needs to be
+		 * 	https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
+		 * 
+		 * SHell Sort:
+		 * 	https://www.toptal.com/developers/sorting-algorithms
+		 * 
+		 * Merge Sort:
+		 * 
+		 */
+		
+	}
+
+}
