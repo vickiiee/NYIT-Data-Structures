7caf8bf7e89873405b0f45ab001c80910b941e17
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-09-26 02:35:46 -0400

    Finished

--- DataStructures260/src/sept24/ArrayIns.java
+++ DataStructures260/src/sept24/ArrayIns.java
@@ -1,19 +1,11 @@
 package sept24;
 
-import java.util.Arrays;
-
 //Vickie Wu
 //9/24/19
 
-
-//insertSort.java
-//demonstrates insertion sort
-//to run this program: C>java InsertSortApp
-//--------------------------------------------------------------
-class ArrayIns {
+public class ArrayIns {
 	private long[] a; // ref to array a
 	private int nElems; // number of data items
-//--------------------------------------------------------------
 
 	public ArrayIns(int max) // constructor
 	{
@@ -21,14 +13,12 @@ class ArrayIns {
 		nElems = 0; // no items yet
 	}
 
-//--------------------------------------------------------------
 	public void insert(long value) // put element into array
 	{
 		a[nElems] = value; // insert it
 		nElems++; // increment size
 	}
 
-//--------------------------------------------------------------
 	public void display() // displays array contents
 	{
 		for (int j = 0; j < nElems; j++) // for each element,
@@ -36,7 +26,6 @@ class ArrayIns {
 		System.out.println("");
 	}
 
-//--------------------------------------------------------------
 	public void insertionSort() {
 		int in, out;
 
@@ -53,60 +42,47 @@ class ArrayIns {
 		} // end for
 	} // end insertionSort()
 //--------------------------------------------------------------
-	
+
+	// -----3.2------------
 	public double median() {
-		insertionSort(); //make sure the arr is already sorted
+		insertionSort(); // make sure the arr is sorted
 		int medIndex;
-		
-		if(nElems%2 == 0) { //even number of elements
-			medIndex = (nElems/2);
-			return ((double) a[medIndex]+ (double) a[medIndex-1])/2;
-		}else { //odd number of elements
-			medIndex = (nElems/2);
+
+		if (nElems % 2 == 0) { // even number of elements
+			medIndex = (nElems / 2);
+			return ((double) a[medIndex] + (double) a[medIndex - 1]) / 2;
+		} else { // odd number of elements
+			medIndex = (nElems / 2);
 			return a[medIndex];
-		} 
+		}
 	}
-	
-	public void noDups() { //CURRENT array must contain no duplicates
-		insertionSort();//make sure the arr is already sorted
-		
-		//linear notation: no nested for loops, skip indexes
-		
+
+	// -----3.3------------
+	public void noDups() { // CURRENT array must contain no duplicates
+		insertionSort();// make sure the arr is sorted
+
+		// O(N): linear notation - no nested for loops to find duplicates so skip indexes:
 		int uniqueNums = 0;
-		
-		//String arr = Arrays.toString(a);
-		
-		//find out how many unique elements there are
-		for(int i = 0; i < nElems; i++) {
-			long input = a[i];
-			
-			if(input != a[i+1] ) {
-				//uniqueNums++;
-				System.out.println("a[i]: "+ a[i]);
-				//noDupArr[index] = a[i];
-				a[uniqueNums] = a[i];
-				uniqueNums++;
+
+		// find out how many unique elements there are
+		for (int i = 0; i < nElems - 1; i++) { // nElems - 1 to avoid outOfBounds error
+
+			// if the next element is not a duplicate
+			if (a[i] != a[i + 1]) {
+				// System.out.println("a[i]: " + a[i]);
+
+				a[uniqueNums] = a[i]; // add the CURRENT element into the correct position in current arr
+				uniqueNums++; // update num of unique elements
 			}
 		}
-		
-		System.out.println("Unique Nums: " +uniqueNums);
-		
-		//make array with the uniqueNUms as length:
-		
-		//long[] noDupArr = new long [uniqueNums];
-		/*int index  = 0;
-		for(int i = 0; i< nElems; i++) {
-			long input = a[i];
-			
-			if(input != a[i+1] ) {
-				System.out.println("a[i]: "+ a[i]);
-				//noDupArr[index] = a[i];
-				a[index] = a[i];
-				index++;
-			}
-		}*/
-		nElems = uniqueNums;
-		//a = noDupArr;
-		System.out.println(Arrays.toString(a)); //dont need no dup arr
+
+		// check last element, which for loop doesnt reach
+		if (a[nElems - 1] != a[uniqueNums - 1]) {
+			a[uniqueNums] = a[nElems - 1];
+			uniqueNums++;
+		}
+
+		nElems = uniqueNums; // update new size
+		// System.out.println("Unique Nums: " + uniqueNums);
 	}
 }
--- DataStructures260/src/sept24/InsertSortApp.java
+++ DataStructures260/src/sept24/InsertSortApp.java
@@ -9,11 +9,11 @@ public class InsertSortApp {
 		ArrayIns arr; // reference to array
 		arr = new ArrayIns(maxSize); // create the array
 
-		arr.insert(77); // insert 10 items
+		arr.insert(77); 
 		arr.insert(99);
 		arr.insert(44);
-		arr.insert(99);
-		arr.insert(55);
+		arr.insert(88);
+		arr.insert(88);
 		arr.insert(22);
 		arr.insert(88);
 		arr.insert(11);
@@ -21,19 +21,25 @@ public class InsertSortApp {
 		arr.insert(66);
 		arr.insert(33);
 		arr.insert(33);
-		
-
-		arr.display(); // display items
-
-		arr.insertionSort(); // insertion-sort them
-
-		arr.noDups();
 		arr.insert(00);
 		arr.insert(66);
 		arr.insert(33);
-		arr.insert(33);
-		arr.display(); // display them again
+		arr.insert(5);
+		arr.insert(99);
+		arr.insert(100);
 		
+		System.out.println("Unsorted array: ");
+		arr.display(); // display items
+
+		arr.insertionSort(); // insertion-sort them
+		System.out.println("\nSorted array: ");
+		arr.display();
+		
+		System.out.println("Median is: " + arr.median());
+		
+		System.out.println("\nTook out duplicates:");
+		arr.noDups();
+		arr.display(); // display them again	
 		System.out.println("Median is: " + arr.median());
 	} // end main()
 } // end class InsertSortApp
