dc68c8e39142df03b4e931c3d40c494f4b863581
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-09-26 15:52:14 -0400

    Stack and a small decimal to binary review

---
+++ DataStructures260/src/sept26/Ch4Notes.java
@@ -0,0 +1,20 @@
+package sept26;
+
+public class Ch4Notes {
+
+	public Ch4Notes() {
+		// TODO Auto-generated constructor stub
+		/**PDF: Ch4: stacks and classes
+		 * 
+		 * 
+		 * Stacks (elements are placed on top of the other)
+		 * 	.push stacks up element values
+		 * 	.pop removes element
+		 * 
+		 * convert decimal to binary:
+		 * 	divide the decimal number by 2 and the remainders of each division will be the binary equivalent of decimal
+		 * 	***look at slide
+		 */
+	}
+
+}
