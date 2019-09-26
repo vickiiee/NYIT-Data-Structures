7486e5a5fcfd4dce56e32af5af1ba5b04b76567b
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-09-26 15:54:15 -0400

    comments

--- DataStructures260/src/sept26/DecimalToBinary.java
+++ DataStructures260/src/sept26/DecimalToBinary.java
@@ -11,17 +11,18 @@ public class DecimalToBinary {
 
 	public DecimalToBinary(long num) {
 		decimal = num;
-		binary = ""; //make sure to assign it to empty or it will be considered null when concatenating : ex: "null01010"
 		System.out.println("Decimal is: " + num);
+		
+		binary = ""; //make sure to assign it to empty or it will be considered null when concatenating : ex: "null01010"
 		remainder = 0;
 		theStack = new StackX(100); // make new stack
 	}
 
 	public void convert() {
 
-		while (decimal >= 1) {
-			remainder = decimal % 2;
-			decimal = decimal / 2;
+		while (decimal >= 1) { //if its not less than 1
+			remainder = decimal % 2; //get remainder which will be pushed to stack
+			decimal = decimal / 2; 
 
 			theStack.push(remainder);
 		}
