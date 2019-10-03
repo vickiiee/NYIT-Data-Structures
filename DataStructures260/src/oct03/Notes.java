ad77bd7814dd58524ea44149ed96fa3a6a77b505
VJ <VJ@216.37.100.193>
2019-10-03 17:03:59 -0400

    linked list notes

---
+++ DataStructures260/src/oct03/Notes.java
@@ -0,0 +1,15 @@
+package oct03;
+
+public class Notes {
+
+	public Notes() {
+		/**
+		 * Linked Lists
+		 * 
+		 * insert element will be placed in the beginning
+		 * 
+		 * delete method will delete specified link and link the previous link to the link after deleted one 
+		 */
+	}
+
+}
