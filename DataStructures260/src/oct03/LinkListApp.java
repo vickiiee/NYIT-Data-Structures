3bbdc1ac34ef44e8c76cca6f86604de409eb0e8c
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-08 01:54:36 -0400

    Finished organizing

--- DataStructures260/src/oct03/LinkListApp.java
+++ DataStructures260/src/oct03/LinkListApp.java
@@ -10,45 +10,33 @@ public class LinkListApp {
 		 */
 		LinkList theList = new LinkList(); // make new list
 
-		theList.insertFirst(10, 2.99); // insert four items
-		theList.insertFirst(10, 4.99);
-		theList.insertFirst(20, 4.99);
-		theList.insertFirst(30, 6);
-		theList.insertFirst(40, 5);
-		theList.insertFirst(50, 3);
-		theList.insertFirst(40, 1);
-		theList.insertFirst(30, 6.99);
-		theList.insertFirst(20, 8.99);
-		theList.insertFirst(10, 4.99);
-		theList.insertFirst(10, 8.99);
+		theList.insertFirst(1, 2.99); // ignore double
+		theList.insertFirst(1, 4.99);
+		theList.insertFirst(2, 4.99);
+		theList.insertFirst(3, 6);
+		theList.insertFirst(4, 5);
+		theList.insertFirst(5, 3);
+		theList.insertFirst(4, 1);
+		theList.insertFirst(3, 6.99);
+		theList.insertFirst(2, 8.99);
+		theList.insertFirst(1, 4.99);
+		theList.insertFirst(1, 8.99);
+		theList.displayList(); 
 		
-		
-		theList.displayList(); // display list
-		System.out.println("RemoveAll(int n): n = 10");
-		//theList.removeLast(30);
-		theList.removeAll(10);
+		System.out.println("\nRemoveAll(int n): n = 1");
+		theList.removeAll(1);
 		theList.displayList();
 		
-		System.out.println("\nRemoveLast(int n): n = 30");
-		theList.removeLast(30);
-		
-		//theList.deleteLast(30);
-		
-		
+		System.out.println("\nRemoveLast(int n): n = 3");
+		theList.removeLast(3);
 		theList.displayList();
 		
-		System.out.println("\nRemoveLast(int n): n = 40");
-		theList.removeLast(40);
+		System.out.println("\nRemoveLast(int n): n = 4");
+		theList.removeLast(4);
 		theList.displayList();
-
-
-		while (!theList.isEmpty()) // until it's empty,
-		{
-			Link aLink = theList.deleteFirst(); // delete link
-			System.out.print("Deleted "); // display it
-			aLink.displayLink();
-			System.out.println("");
-		}
-		theList.displayList(); // display list
+		
+		System.out.println("\nRemoveAll(int n): n = 2");
+		theList.removeAll(2);
+		theList.displayList();
 	} // end main()
 } 
