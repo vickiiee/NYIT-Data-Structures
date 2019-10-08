9f7e70dc00d71c3762414104fe8599d120797c2f
VJ <VJ@216.37.102.147>
2019-10-08 15:17:09 -0400

    Linked List

---
+++ DataStructures260/src/oct08/Notes.java
@@ -0,0 +1,19 @@
+package oct08;
+
+public class Notes {
+
+	public Notes() {
+		/**
+		 * Check out linked list zybooks because its basically what we are learning in class
+		 * 
+		 * PDF: ch5
+		 * 
+		 * LinkedList vs Araray LIst
+		 * 
+		 * Stack is sbstract because we can put different types of stuff in it: arrays, ints, etc.
+		 * 
+		 * linked list 
+		 */
+	}
+
+}
