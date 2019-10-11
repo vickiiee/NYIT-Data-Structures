8c452f58eb3fb8286b19eb147a5f2006a7005038
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-10 23:04:59 -0400

    link succese

--- DataStructures260/src/oct10/PriorityLinkQueue.java
+++ DataStructures260/src/oct10/PriorityLinkQueue.java
@@ -11,32 +11,53 @@ public class PriorityLinkQueue extends DoublyLinkedList{
 	}
 	
 	public void insertSort(long key) { //or insert long and make new link
+		System.out.println("\n	method called: "+ key);
 		//smallest -> biggest
 		//0,2,5,7,9
 		//key = 8
 	
 		if(isEmpty()) {
+			System.out.println("	isEmpty");
 			insertFirst(key);
 		}else {
+			
 			Link current = getFirst();
 			long data = current.dData;
-			while(current != null && data<key) {
+			while(current != null ) {
+				//0,2,5,7,9
+				//key = 8
+				System.out.println("		while loop");
 				//long previous = current.previous.dData;
 				data = current.dData;
 				
-				if(data>key) {
+				if(data>=key) {
 					/*inset before
 					 * make new pathway to new link
 					 */
 					if(current != getFirst()) {
-						Link newLink = new Link(key);
+						/*Link newLink = new Link(key);
 						newLink.next = current;
+						newLink.previous = current.previous;*/
+						Link newLink = new Link(key);
+						
+						current.previous.next = newLink;
 						newLink.previous = current.previous;
+						
+						newLink.next = current;
+						current.previous = newLink;
+						
+						
+						
+						//newLink.previous.next = newLink;
+			
+						current.previous = newLink;
 						return;
 					}else {
-						Link newLink = new Link(key);
+						/*Link newLink = new Link(key);
 						newLink.next = getFirst();
 						setFirst(newLink);
+						*/
+						insertFirst(key);
 						return;
 					}
 				}
