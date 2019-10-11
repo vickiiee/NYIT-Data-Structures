eb0b8c012f9e9ed6379995167b6aa8c810ed8e99
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-11 03:37:24 -0400

    extends DoublyLinkedList

--- DataStructures260/src/oct10/PriorityLinkQueue.java
+++ DataStructures260/src/oct10/PriorityLinkQueue.java
@@ -1,54 +1,55 @@
 package oct10;
 
-public class PriorityLinkQueue extends DoublyLinkedList{
+public class PriorityLinkQueue extends DoublyLinkedList {
 
-	//sorted linked list
+	// sorted linked list
+	
 	public PriorityLinkQueue() {
 
 	}
-	
-	public void insertSort(long key) { //or insert long and make new link
-		System.out.println("\n	method called: "+ key);
-		//smallest -> biggest
-		//0,2,5,7,9
-		//key = 8
-	
-		if(isEmpty()) {
+
+	public void insertSort(long key) { // or insert long and make new link
+		System.out.println("\n	method called: " + key);
+		// smallest -> biggest
+		// 0,2,5,7,9
+		// key = 8
+
+		if (isEmpty()) {
 			System.out.println("	isEmpty");
 			insertFirst(key);
-		}else {
-			
+		} else {
+
 			Link current = getFirst();
 			long data = current.dData;
-			while(current != null ) {
-				//0,2,5,7,9
-				//key = 8
+			while (current != null) {
+				// 0,2,5,7,9
+				// key = 8
 				System.out.println("		while loop");
-				//long previous = current.previous.dData;
+				// long previous = current.previous.dData;
 				data = current.dData;
-				
-				if(data>=key) {
-					/*inset before
-					 * make new pathway to new link
-					 */
-					if(current != getFirst()) {
-						/*Link newLink = new Link(key);
-						newLink.next = current;
-						newLink.previous = current.previous;*/
+
+				if (data >= key) {
+					// insert before
+					// make new pathway to new link
+
+					if (current != getFirst()) {
+						/*
+						 * Link newLink = new Link(key); newLink.next = current; newLink.previous =
+						 * current.previous;
+						 */
 						Link newLink = new Link(key);
-						
+
 						current.previous.next = newLink;
 						newLink.previous = current.previous;
-						
+
 						newLink.next = current;
 						current.previous = newLink;
-						
+
 						return;
-					}else {
-						/*Link newLink = new Link(key);
-						newLink.next = getFirst();
-						setFirst(newLink);
-						*/
+					} else {
+						/*
+						 * Link newLink = new Link(key); newLink.next = getFirst(); setFirst(newLink);
+						 */
 						insertFirst(key);
 						return;
 					}
@@ -57,15 +58,15 @@ public class PriorityLinkQueue extends DoublyLinkedList{
 			}
 			insertLast(key);
 		}
-				
+
 	}
-	
+
 	public long remove() {
-		//just remove last link because if its sorted it should already have the highest in the lst
+		// just remove last link because if its sorted it should already have the
+		// highest in the lst
 		Link l = deleteLast();
 		long value = l.dData;
 		return value;
 	}
 
-	
 }
