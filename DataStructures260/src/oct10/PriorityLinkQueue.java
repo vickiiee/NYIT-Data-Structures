92b5dae0f3442c8a9b8bc2185ce5db0e2d4471ee
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-11 03:45:06 -0400

    without extends DoublyLinkedList

--- DataStructures260/src/oct10/PriorityLinkQueue.java
+++ DataStructures260/src/oct10/PriorityLinkQueue.java
@@ -1,9 +1,9 @@
 package oct10;
 
-public class PriorityLinkQueue extends DoublyLinkedList {
+public class PriorityLinkQueue{
 
 	// sorted linked list
-	
+	/*
 	public PriorityLinkQueue() {
 
 	}
@@ -33,10 +33,7 @@ public class PriorityLinkQueue extends DoublyLinkedList {
 					// make new pathway to new link
 
 					if (current != getFirst()) {
-						/*
-						 * Link newLink = new Link(key); newLink.next = current; newLink.previous =
-						 * current.previous;
-						 */
+						
 						Link newLink = new Link(key);
 
 						current.previous.next = newLink;
@@ -47,9 +44,6 @@ public class PriorityLinkQueue extends DoublyLinkedList {
 
 						return;
 					} else {
-						/*
-						 * Link newLink = new Link(key); newLink.next = getFirst(); setFirst(newLink);
-						 */
 						insertFirst(key);
 						return;
 					}
@@ -67,6 +61,82 @@ public class PriorityLinkQueue extends DoublyLinkedList {
 		Link l = deleteLast();
 		long value = l.dData;
 		return value;
+	}
+	*/
+	
+	private DoublyLinkedList list;
+	
+	public PriorityLinkQueue() {
+		list = new DoublyLinkedList();
+	}
+
+	public void insertSort(long key) { // or insert long and make new link
+		System.out.println("\n	method called: " + key);
+		// smallest -> biggest
+		// 0,2,5,7,9
+		// key = 8
+
+		if (list.isEmpty()) {
+			System.out.println("	isEmpty");
+			list.insertFirst(key);
+		} else {
+
+			Link current = list.getFirst();
+			long data = current.dData;
+			while (current != null) {
+				// 0,2,5,7,9
+				// key = 8
+				System.out.println("		while loop");
+				// long previous = current.previous.dData;
+				data = current.dData;
+
+				if (data >= key) {
+					// insert before
+					// make new pathway to new link
+
+					if (current != list.getFirst()) {
+						/*
+						 * Link newLink = new Link(key); newLink.next = current; newLink.previous =
+						 * current.previous;
+						 */
+						Link newLink = new Link(key);
+
+						current.previous.next = newLink;
+						newLink.previous = current.previous;
+
+						newLink.next = current;
+						current.previous = newLink;
+
+						return;
+					} else {
+						/*
+						 * Link newLink = new Link(key); newLink.next = getFirst(); setFirst(newLink);
+						 */
+						list.insertFirst(key);
+						return;
+					}
+				}
+				current = current.next;
+			}
+			list.insertLast(key);
+		}
+
+	}
+
+	public long remove() {
+		// just remove last link because if its sorted it should already have the
+		// highest in the lst
+		Link l = list.deleteLast();
+		long value = l.dData;
+		return value;
+	}
+	
+	public void displayForward() {
+		list.displayForward();
+	}
+	
+	public void displayBackward() {
+		list.displayBackward();
 	}
 
 }
