5f950b3ab329c0eb4007589d44539c3c27fea391
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-11 03:58:21 -0400

    generated getter/setters

--- DataStructures260/src/oct10/Link.java
+++ DataStructures260/src/oct10/Link.java
@@ -1,9 +1,11 @@
 package oct10;
 
+
+
 public class Link {
-	public long dData; // data item
-	public Link next; // next link in list
-	public Link previous; // previous link in list
+	private long dData; // data item
+	private Link next; // next link in list
+	private Link previous; // previous link in list
 	// -------------------------------------------------------------
 
 	public Link(long d) // constructor
@@ -17,4 +19,28 @@ public class Link {
 		System.out.print(dData + " ");
 	}
 	// -------------------------------------------------------------
+	
+	public long getdData() {
+		return dData;
+	}
+
+	public void setdData(long dData) {
+		this.dData = dData;
+	}
+
+	public Link getNext() {
+		return next;
+	}
+
+	public void setNext(Link next) {
+		this.next = next;
+	}
+
+	public Link getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Link previous) {
+		this.previous = previous;
+	}
 }
