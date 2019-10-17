07fabb6109b694d431bde966a7b3728173ef6739
VJ <VJ@216.37.101.176>
2019-10-17 15:19:20 -0400

    midterm review guidelines

---
+++ DataStructures260/src/oct17midtermReview/Notes.java
@@ -0,0 +1,36 @@
+package oct17midtermReview;
+
+public class Notes {
+	/*
+	 * check out review pdf 
+	 * 
+	 * Ch2 Arrays 
+	 * • The Basics of Arrays in Java (search, insert, delete) 
+	 * • Java Code for an Ordered Array (searching: linear, binary) (noDups()) 
+	 * • Logarithms (how many steps are required to binary search and array of 1000 elements?) 
+	 * • Big O Notation (Running Time in Big O Notation)
+	 * 
+	 * Ch3 Simple sorting 
+	 * • Bubble Sort (77 99 44 55 show the steps) 
+	 * • Selection Sort (same) 
+	 * • Insertion Sort (same)
+	 * 
+	 * Ch4 Stacks and Queues (with arrays) 
+	 * • (12 3 4 * 5 * * 9 * 10) Numbers mean a push on a stack and * means a pop. What is the final stack? 
+	 * • (12 3 4 * 5 * * 9 * 10) Numbers mean a add to a queue and * means remove. What is the final queue? 
+	 * • Stacks (write a stack class with array/link list) 
+	 * • Queues (write a queue class with array/link list) 
+	 * • Priority Queues 
+	 * • Given a partial class, write constructor, remove(), peek(), isEmpty(), isFull(), traverse() + other methods.
+	 * 
+	 * Ch 5 Linked Lists 
+	 * • Links 
+	 * • A Simple Linked List 
+	 * • Finding and Deleting Specified Links 
+	 * • Double-Ended List 
+	 * • Doubly-Linked List 
+	 * • Given a partial class, write constructor, insertFirst(), deleteFirst(),, insertLast(), deleteLast(), find() , displayList() , traverse() + other methods.
+	 * 
+	 * Ch6 Recursion
+	 */
+}
