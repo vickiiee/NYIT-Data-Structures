1b0fca2aef3b7559641c7261d5592b2e07c6a4b8
VJ <VJ@216.37.101.176>
2019-10-16 20:41:06 -0400

    recursion exercises completed

--- DataStructures260/src/oct15/RecursionExercises.java
+++ DataStructures260/src/oct15/RecursionExercises.java
@@ -11,6 +11,14 @@ public class RecursionExercises {
 		System.out.println(s.rangeSum(1, 5));
 		System.out.println(s.rangeSum(-3,4));
 		
+		System.out.println("\n"+s.countX("xxxxxxxx"));
+		String p = "dcv";
+		System.out.println("\n"+s.countHi("xxxhixhixxxhix"));
+		System.out.println(p.substring(2));
+		
+		System.out.println("\n"+s.changeXY("xxxhixhixxxhix"));
+		System.out.println(p.substring(2));
+		
 	}
 
 	/*
@@ -27,4 +35,43 @@ rangeSum(-3,4) -> -3+-2+-1+0+1+2+3+4=4
 		}
 		return end + rangeSum(start,end-1);
 	}
+	
+/*
+ * 8- Given a string, compute recursively (no loops) the number of lowercase &#39;x&#39; chars in the string.
+countX(xxhixx;) = 4
+countX(xhixhix) = 3
+countX(&quot;hi&quot;) 
+ */
+	//Ex8
+	public int countX(String s) {
+		int x = s.indexOf("x");
+		if(x== -1) {
+			return 0;
+		}else {
+			return 1+countX(s.substring(x+1));
+		}
+	}
+	
+	/*
+	 * countHi(&quot;xxhixx&quot;): 1
+countHi(&quot;xhixhix&quot;): 2
+countHi(&quot;hi&quot;) : 1
+	 */
+	//Ex 9
+	public int countHi(String c) {
+		if(c.indexOf("hi")==-1) {
+			return 0;
+		}else
+			return 1+ countHi(c.substring(c.indexOf("hi")+1));
+	}
+	
+	//EX 10:
+	public String changeXY(String s) {
+		if(s.indexOf("x")==-1) {
+			return "";
+		}else {
+			return s.substring(0, s.indexOf("x"))+"y"+changeXY(s.substring(s.indexOf("x")+1));
+		}
+	}
 }
+
