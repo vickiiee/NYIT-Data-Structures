5744f357890ae92ee3773390f86bbf0d7f320962
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-20 20:34:33 -0400

    big o notation

--- DataStructures260/src/oct17midtermReview/Notes.java
+++ DataStructures260/src/oct17midtermReview/Notes.java
@@ -6,14 +6,53 @@ public class Notes {
 	 * 
 	 * Ch2 Arrays 
 	 * • The Basics of Arrays in Java (search, insert, delete) 
+	 * 		>Search: using a for loop
+	 * 		>Insert: using a for loop and shifting values up
+	 * 		>Delete: using a for loop and shifting values down
 	 * • Java Code for an Ordered Array (searching: linear, binary) (noDups()) 
+	 * 		>Linear Search: for loop and search through the entire array
+	 * 
+	 * 			for(int i = 0; i < n; i++){
+	 * 				if(a[i]== key) return a[i]; or print "found"
+	 * 			}
+	 * 
+	 * 		>Binary Search: while/recursion to search through halves of the array
+	 * 
+	 * 			low = 0;
+	 * 			high = a.length-1;
+	 * 			while(high>=low){
+	 * 				mid = (low + high)/2;
+	 * 				if(a[mid]==key){
+	 * 					print found!!
+	 * 					return mid;
+	 * 				}else if (a[mid] > key){
+	 * 					high = mid - 1;
+	 * 				}else {
+	 * 					low = mid + 1;
+	 * 				}
+	 * 			}
+	 * 			return -1;
+	 * 
+	 * 		>Check out noDups()
+	 * 
 	 * • Logarithms (how many steps are required to binary search and array of 1000 (N) elements?) 
-	 * 		>Check out Zybooks 2.2
+	 * 		>ZYBOOKS 2.2
 	 * 		>Binary Search: [log2 (N) + 1] = number of STEPS required if cannot find the search element. 
-	 * 						[log2 (N)] = number of elements checked if cannot find the search element
+	 * 						[log2 (N)] = number of ELEMENTS CHECKED if cannot find the search element
 	 * 		>Linear Search: N times if cannot find the search element
 	 * • Big O Notation (Running Time in Big O Notation)
-	 * 
+	 * 		>Check out WS/Zybooks 2.5
+	 * 		>O(1): Constant
+	 * 		>O(logN): Logarithmic
+	 * 			*Binary Search
+	 * 		>O(N): Linear
+	 * 			*Linear Search
+	 * 		>O(NlogN): Linearithmic
+	 * 			*Merge Sort
+	 * 		>O(N^2): Quadratic
+	 * 			*Selection Sort
+	 * 		>O(C^N): Exponential
+	 * 			*Fibonacchi
 	 * Ch3 Simple sorting 
 	 * • Bubble Sort (77 99 44 55 show the steps) 
 	 * • Selection Sort (same) 
@@ -37,4 +76,20 @@ public class Notes {
 	 * 
 	 * Ch6 Recursion
 	 */
+	
+	public static void main(String args[]) {
+		//Big O Notation:
+		
+		//Ex 6:
+		int sum = 0;
+		int n = 10;
+		for(int i = 0; i < n; i++) {
+			System.out.println(i);
+			for(int j =1; j < n; j*=2) {
+				System.out.println("	"+j);
+				sum++;
+				System.out.println("		"+sum);
+			}
+		}
+	}
 }
