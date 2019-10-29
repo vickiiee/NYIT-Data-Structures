b08f8c7f889778faf429ee16cb9919339f7ef972
VJ <VJ@216.37.97.41>
2019-10-29 15:29:00 -0400

    original code

---
+++ DataStructures260/src/oct29/Node.java
@@ -0,0 +1,24 @@
+package oct29;
+
+// tree.java
+// demonstrates binary tree
+import java.io.*;
+import java.util.*; // for Stack class
+////////////////////////////////////////////////////////////////
+
+class Node {
+	public int iData; // data item (key)
+	public double dData; // data item
+	public Node leftChild; // this node's left child
+	public Node rightChild; // this node's right child
+
+	public void displayNode() // display ourself
+	{
+		System.out.print('{');
+		System.out.print(iData);
+		System.out.print(", ");
+		System.out.print(dData);
+		System.out.print("} ");
+	}
+} // end class Node
+////////////////////////////////////////////////////////////////
