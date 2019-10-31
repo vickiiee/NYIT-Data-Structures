8b5a68f0dd5a6a682d24b3f6f8e70052c66b8462
VJ <VJ@216.37.97.41>
2019-10-30 23:07:54 -0400

    laggy

--- DataStructures260/src/oct29/TreeApp.java
+++ DataStructures260/src/oct29/TreeApp.java
@@ -3,12 +3,19 @@ package oct29;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+	
 class TreeApp {
+	private static Tree theTree;
+	private static int minimum;
+	private static int max;
+	private static int rightMinValue;
+	private static int leftMaxValue;
+	
+	
 	public static void main(String[] args) throws IOException {
 		int value;
-		Tree theTree = new Tree();
-
+		 theTree= new Tree();
+		boolean start = true;
 		//hw
 		theTree.insert(7, 1.5);
 		theTree.insert(5, 1.2);
@@ -34,9 +41,12 @@ class TreeApp {
 		theTree.insert(93, 1.5);
 		theTree.insert(97, 1.5);*/
 
-		while (true) {
-			System.out.print("Enter first letter of show, ");
-			System.out.print("insert, find, delete, or traverse: ");
+		while (start) {
+			System.out.println("Enter first letter of show, insert, find, delete, or traverse, OR");
+			System.out.println("Enter z for size, p for depth, x for max, n for min, v for removeLeaves, r for rightMin, l for leftMinValue, q for quit");
+			
+			//si(z)e, de(p)th,
+					//ma(x), mi(n), removelLea(v)es, (r)ightMin, (l)eftMinValue, (q)uit:
 			int choice = getChar();
 			switch (choice) {
 			case 's':
@@ -66,7 +76,7 @@ class TreeApp {
 				if (didDelete)
 					System.out.print("Deleted " + value + '\n');
 				else
-					System.out.print("Could not delete ");
+					System.out.print("Could not delete \n");
 				System.out.print(value + '\n');
 				break;
 			case 't':
@@ -74,12 +84,108 @@ class TreeApp {
 				value = getInt();
 				theTree.traverse(value);
 				break;
+			case 'z':
+				//size(): total nodes in binary search tree
+				System.out.println("There are "+size(theTree.getRoot())+ " nodes.\n");
+				break;
+			case 'p':
+				//depth(key): height //check parents?
+				break;
+			case 'x':
+				//max: return max node
+				max = theTree.getRoot().iData;
+				System.out.print("Maximum: "+maximum(theTree.getRoot().rightChild)+"\n");
+				break;
+			case 'n':
+				//min : return min mode
+				 minimum = theTree.getRoot().iData;
+				System.out.print("Minimum: "+minimum(theTree.getRoot().leftChild)+"\n");
+				break;
+			case 'v':
+				//removeLeaves
+				break;
+			case 'r':
+				//rightMin: minimum value of right subtree
+				rightMinValue = theTree.getRoot().rightChild.iData;
+				System.out.print("Right Minimum Value: "+rightMinValue(theTree.getRoot().rightChild)+"\n");
+				break;
+			case 'l':
+				//leftMin: minimum vaue of left subtree
+				leftMaxValue = theTree.getRoot().leftChild.iData;
+				System.out.print("Left Maximum Value: "+leftMaxValue(theTree.getRoot().leftChild)+"\n");
+				break;
+			case 'q':
+				//quit
+				System.out.print("Quit");
+				start = false;
+				break;
 			default:
 				System.out.print("Invalid entry\n");
 			} // end switch
 		} // end while
 	} // end main()
 // -------------------------------------------------------------
+	public static int size(Node root)  
+	{  //preorder
+	    if (root == null)  
+	        return 0;  
+	    
+	    System.out.println("Node "+ root.iData);
+	    return 1 +size(root.leftChild) +  size(root.rightChild);  
+	} 
+	
+	public static int minimum(Node rootLeftChild) {
+		if(rootLeftChild != null) {
+			//System.out.println("mini: "+minimum+ " data:"+rootLeftChild.iData );
+			if(rootLeftChild.iData<minimum) {
+				minimum = rootLeftChild.iData;
+			}
+			
+			minimum(rootLeftChild.leftChild);
+			minimum(rootLeftChild.rightChild);
+		}
+		return minimum;
+	}
+	
+	public static int maximum(Node rootRightChild) {
+		if(rootRightChild != null) {
+			//System.out.println("mini: "+minimum+ " data:"+rootLeftChild.iData );
+			if(rootRightChild.iData>max) {
+				max = rootRightChild.iData;
+			}
+			
+			maximum(rootRightChild.leftChild);
+			maximum(rootRightChild.rightChild);
+		}
+		return max;
+	}
+	
+	public static int rightMinValue(Node rootRightChild) {
+		if(rootRightChild != null) {
+			System.out.println("rughtmini: "+rightMinValue+ " data:"+rootRightChild.iData );
+			if(rootRightChild.iData<rightMinValue) {
+				rightMinValue = rootRightChild.iData;
+			}
+			
+			rightMinValue(rootRightChild.leftChild);
+			rightMinValue(rootRightChild.rightChild);
+		}
+		return rightMinValue;
+	}
+	
+	public static int leftMaxValue(Node rootLeftChild) {
+		if(rootLeftChild != null) {
+			System.out.println("leftmax: "+leftMaxValue+ " data:"+rootLeftChild.iData );
+			if(rootLeftChild.iData>leftMaxValue) {
+				leftMaxValue = rootLeftChild.iData;
+			}
+			
+			leftMaxValue(rootLeftChild.leftChild);
+			leftMaxValue(rootLeftChild.rightChild);
+		}
+		return leftMaxValue;
+	}
+	//----------------------------------------------------------------------
 
 	public static String getString() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
