b2b27114bca8a3616dbb999363117b93bb4ecd56
VJ <VJ@LAPTOP-P9U4FI6A.fios-router.home>
2019-10-31 02:42:03 -0400

    BST delete notes

---
+++ DataStructures260/src/oct29/Notes.java
@@ -0,0 +1,12 @@
+package oct29;
+
+public class Notes {
+	/*BINARY SEARCH TREE
+	 * Deletind nodes:
+	 * 
+	 * 	3 cases: 
+	 * 		- delteing parent with children
+	 * 		-find sucessor 
+	 * 		- check out slide#90
+	 */
+}
