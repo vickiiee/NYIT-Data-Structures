300c81e8e09c2a9cc32828ff3b21714fdfc8937a
VJ <VJ@216.37.98.137>
2019-11-14 14:35:28 -0500

    graph

--- DataStructures260/src/nov07/NOtes.java
+++ DataStructures260/src/nov07/NOtes.java
@@ -1,9 +1,17 @@
 package nov07;
 
 public class NOtes {
-/*
- * CH 13 graphs:
- * 
- * 
- */
+	/*
+	 * CH 13 graphs: Zybooks: A graph is a data structure for representing
+	 * connections among items, and consists of vertices connected by edges.
+	 * 
+	 * A vertex (or node) represents an item in a graph. 
+	 * An edge represents a connection between two vertices in a graph.
+	 * 
+	 * In a graph:
+
+Two vertices are adjacent if connected by an edge.
+A path is a sequence of edges leading from a source (starting) vertex to a destination (ending) vertex. The path length is the number of edges in the path.
+The distance between two vertices is the number of edges on the shortest path between those vertices.
+	 */
 }
