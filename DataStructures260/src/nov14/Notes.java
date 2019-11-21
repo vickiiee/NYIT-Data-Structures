312c1048027b3bf7248cdd918de0c229801d1543
VJ <VJ@216.37.97.49>
2019-11-21 14:21:22 -0500

    class notes

---
+++ DataStructures260/src/nov14/Notes.java
@@ -0,0 +1,25 @@
+package nov14;
+
+public class Notes {
+/*
+ * Graphs: 
+ * 	Topological order
+ * 
+ * Warshall’s Algorithm
+ * 
+ * Ch 14 Weighted Graphs
+ * 
+ * Minimum Spanning Tree with Weighted Graphs
+When all edges are the same weight, it’s fairly straightforward—
+as we saw earlier—for the algorithm to choose one to add to
+the minimum spanning tree. But when edges can have
+different weights, some arithmetic is needed to choose the
+right one.,
+
+******************important****
+krusgal's algorithm:
+consider adding edges based on weight, unless it creates a cycle: slide 137
+
+prims algorithm: like breadth first search, look at adjacent vertexes first. if it is the shortest then add it into graph.
+ */
+}
---
+++ DataStructures260/src/nov19/Notes.java
@@ -0,0 +1,9 @@
+package nov19;
+
+public class Notes {
+
+	/*Dijstra’s shortest path algorithm,
+	 * shortest path:
+	 * shortest path from one vertex to all vertex
+	 */
+}
